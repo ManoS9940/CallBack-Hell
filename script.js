3994d2f2e9002e68b33d5d78cb7410c3f6a30e93
Mano S <manokcg@gmail.com>
2022-08-02 22:11:23 +0530

    callback hell

---
+++ script.js
@@ -0,0 +1,35 @@
+const countdown = document.querySelector('#countdown');
+
+setTimeout(function(){
+    countdown.innerText ='10';
+    setTimeout(function(){
+        countdown.innerText = '9';
+        setTimeout(function(){
+            countdown.innerText = '8';
+            setTimeout(function(){
+                countdown.innerText = '7';
+                setTimeout(function(){
+                    countdown.innerText = '6';
+                    setTimeout(function(){
+                        countdown.innerText = '5';
+                        setTimeout(function(){
+                            countdown.innerText = '4';
+                            setTimeout(function(){
+                                countdown.innerText = '3';
+                                setTimeout(function(){
+                                    countdown.innerText = '2';
+                                    setTimeout(function(){
+                                        countdown.innerText = '1';
+                                        setTimeout(function(){
+                                            countdown.innerText = 'Happy Independence Day!'
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+},1000)
